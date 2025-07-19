8983e5865a324c12947ed2a783188a0c3c11a45b
alda-1995 <aldair.canalla.agency@gmail.com>
2025-07-19 12:20:42 -0600

    se cambia dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,27 +4,27 @@
   <div class="flex flex-wrap justify-between gap-3 py-4">
     <div class="flex min-w-72 flex-col gap-3">
       <p class="text-[#111418] tracking-light text-[32px] font-bold leading-tight">Dashboard</p>
-      <p class="text-[#60758a] text-sm font-normal leading-normal">Overview of key metrics and usage data</p>
+      <p class="text-[#60758a] text-sm font-normal leading-normal">Descripción general de las métricas clave y los datos de uso</p>
     </div>
   </div>
-  <div class="flex flex-wrap gap-4 py-4">
-    <div class="flex min-w-[158px] flex-1 flex-col gap-2 rounded-lg p-6 border border-[#dbe0e6]">
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-4">
+    <!-- <div class="flex min-w-[158px] flex-1 flex-col gap-2 rounded-lg p-6 border border-[#dbe0e6]">
       <p class="text-[#111418] text-base font-medium leading-normal">Total Users</p>
       <p class="text-[#111418] tracking-light text-2xl font-bold leading-tight">1,250</p>
     </div>
     <div class="flex min-w-[158px] flex-1 flex-col gap-2 rounded-lg p-6 border border-[#dbe0e6]">
       <p class="text-[#111418] text-base font-medium leading-normal">Active Users (Last 30 Days)</p>
       <p class="text-[#557caf] tracking-light text-2xl font-bold leading-tight">875</p>
-    </div>
-    <div class="flex min-w-[158px] flex-1 flex-col gap-2 rounded-lg p-6 border border-[#dbe0e6]">
-      <p class="text-[#111418] text-base font-medium leading-normal">Notifications Sent (Last 7 Days)</p>
-      <p class="text-[#111418] tracking-light text-2xl font-bold leading-tight">3,420</p>
+    </div> -->
+    <div class="flex min-w-[158px] flex-col gap-2 rounded-lg p-6 border border-[#dbe0e6]">
+      <p class="text-[#111418] text-base font-medium leading-normal">Notificaciones enviadas (últimos 7 días)</p>
+      <p class="text-[#111418] tracking-light text-2xl font-bold leading-tight">3</p>
     </div>
   </div>
-  <h2 class="text-[#111418] text-[22px] font-bold leading-tight tracking-[-0.015em] pb-3 pt-5">Notification Channels Usage</h2>
+  <h2 class="text-[#111418] text-[22px] font-bold leading-tight tracking-[-0.015em] pb-3 pt-5">Uso de los canales de notificación</h2>
   <div class="flex flex-wrap gap-4 py-6">
     <div class="flex min-w-72 flex-1 flex-col gap-2 rounded-lg border border-[#dbe0e6] p-6">
-      <p class="text-[#111418] text-base font-medium leading-normal">Channel Usage</p>
+      <p class="text-[#111418] text-base font-medium leading-normal">Uso del canal</p>
       <div class="grid min-h-[180px] gap-x-4 gap-y-6 grid-cols-[auto_1fr] items-center py-3">
         <p class="text-[#60758a] text-[13px] font-bold leading-normal tracking-[0.015em]">SMS</p>
         <div class="h-full flex-1">
@@ -41,10 +41,10 @@
       </div>
     </div>
   </div>
-  <h2 class="text-[#111418] text-[22px] font-bold leading-tight tracking-[-0.015em] pb-3 pt-5">User Engagement</h2>
+  <h2 class="text-[#111418] text-[22px] font-bold leading-tight tracking-[-0.015em] pb-3 pt-5">Uso de la app</h2>
   <div class="flex flex-wrap gap-4 py-6">
     <div class="flex min-w-72 flex-1 flex-col gap-2 rounded-lg border border-[#dbe0e6] p-6">
-      <p class="text-[#111418] text-base font-medium leading-normal">User Activity Over Time</p>
+      <p class="text-[#111418] text-base font-medium leading-normal">Actividad a lo largo del tiempo</p>
       <div class="flex min-h-[180px] flex-1 flex-col gap-8 py-4">
         <svg width="100%" height="148" viewBox="-3 0 478 150" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
           <path
